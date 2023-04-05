ae461069fd70ec351f09742b0dfc4a9a59807268
Utif Milkedori <utifmd@gmail.com>
2023-04-05 14:59:22 +0700

    migrate then enums

---
+++ src/enum.ts
@@ -0,0 +1,12 @@
+enum Directions {
+    NORTH = "NORTH",
+    SOUTH = "SOUTH",
+    WEST = "WEST",
+    EAST = "EAST"
+}
+console.log(Directions.NORTH)
+
+enum Status {
+    OFF, ON
+}
+console.log(Status.OFF);
